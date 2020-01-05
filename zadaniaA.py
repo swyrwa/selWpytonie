ca910ae072e6dd254f66b1734e0614d4076cdfba
swyrwa <slawa.wyrwa@gmail.com>
2020-01-05 22:41:54 +0100

    zadania a do 5

--- zadaniaA.py
+++ zadaniaA.py
@@ -20,18 +20,45 @@
 
 # zad3
 # silnia rekurencyjna
-# def silniaRekurencyjna(liczba)
+# def silniaRekurencyjna(liczba):
+#     if liczba == 1:
+#         wynik = liczba
+#         print(wynik)
+#         return wynik
+#     else:
+#         wynik = liczba * silniaRekurencyjna(liczba - 1)
+#         print(wynik)
+#         return wynik
+#
+#
+# silniaRekurencyjna(4)
 
 # zad4
 # choinka
-def rysujChoinke(wysokosc):
-    x = 1
-    y = wysokosc
-    while x <= wysokosc:
-        print((y-1)*' ' + x*'*' + (y-1)*' ')
-        y = y - x
-        x = x + 1
+# def rysujChoinke(wysokosc):
+#     x = 1
+#     y = wysokosc
+#     z = 1
+#     while z <= wysokosc:
+#         print((y-1)*' ' + x*'*' + (y-1)*' ')
+#         y = y - 1
+#         x = x + 2
+#         z = z + 1
+#
+# rysujChoinke(4)
 
-
-
-rysujChoinke(8)
+#zad5
+# sprawdzanie czy liczba jest pierwsza
+#
+# def czyLiczbaPierwsza():
+#     liczba = int(input("Podaj liczbę: "))
+#     dzielniki = list()
+#     for i in range(2, liczba-1):
+#         if liczba % i == 0:
+#             dzielniki.append(i)
+#     if len(dzielniki) != 0:
+#         print(dzielniki)
+#         return print("{} to nie jest liczba pierwsza.".format(liczba))
+#     else:
+#         return print("{} jest liczbą pierwszą.".format(liczba))
+# czyLiczbaPierwsza()
